6137ce6d1bcf882ec8a3af8eb36a450b8a565693
jenkins <jenkins@teamw.in>
2016-02-11 14:59:27 -0500

    add f1fs

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 3 */
+/* This file is auto generated, version 5 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#3 SMP PREEMPT Sun Feb 7 14:46:18 EST 2016"
+#define UTS_VERSION "#5 SMP PREEMPT Thu Feb 11 14:56:24 EST 2016"
 #define LINUX_COMPILE_BY "jenkins"
 #define LINUX_COMPILE_HOST "build"
 #define LINUX_COMPILER "gcc version 4.7 (GCC) "
