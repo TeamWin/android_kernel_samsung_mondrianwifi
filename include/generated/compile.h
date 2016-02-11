eb9cd0fee93769e2e6ff85bed59516586dc120da
jenkins <jenkins@teamw.in>
2016-02-11 13:29:15 -0500

    initial import

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 SMP PREEMPT Sun Feb 7 14:46:18 EST 2016"
+#define LINUX_COMPILE_BY "jenkins"
+#define LINUX_COMPILE_HOST "build"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
