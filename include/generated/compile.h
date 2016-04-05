9314e1a108e14446a4e465b0f8afab964f8e2c9f
jenkins <jenkins@teamw.in>
2016-04-05 18:22:53 -0400

    Update f2fs

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 5 */
+/* This file is auto generated, version 6 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#5 SMP PREEMPT Thu Feb 11 14:56:24 EST 2016"
+#define UTS_VERSION "#6 SMP PREEMPT Thu Feb 11 15:44:20 EST 2016"
 #define LINUX_COMPILE_BY "jenkins"
 #define LINUX_COMPILE_HOST "build"
 #define LINUX_COMPILER "gcc version 4.7 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0-gdff76d3-dirty"
+#define UTS_RELEASE "3.4.0-g6137ce6-dirty"
